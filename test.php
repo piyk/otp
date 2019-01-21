85f1a253bee473e567c0d2ff5a9e135c1bdd5acb
piyk <42693110+piyk@users.noreply.github.com>
2019-01-21 13:21:41 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+    require_once('lib/otphp.php');
+
+    $totp = new \OTPHP\TOTP("JDDK4U6G3BJLEZ7Y");
+    $chl = $totp->provisioning_uri("piyawad.k@udru.ac.th"); 
+    echo "url  ".$chl;
+
+    // 'https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/piyawad?secret=JDDK4U6G3BJLEZ7Y
+
+    echo "<br>Current OTP: ". $totp->now();
