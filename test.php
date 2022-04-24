a4d9eea817453de25460f68f3748b3d0914e8896
Piyawad Kasabai <42693110+piyk@users.noreply.github.com>
2022-04-24 12:56:01 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
     require_once('otphp/lib/otphp.php');
 
-    $totp = new \OTPHP\TOTP("JDDK4U6G3BJLEZ7Y");
-    $chl = $totp->provisioning_uri("piyawad.k@udru.ac.th"); 
+    $secretcode  = "JDDK4U6G3BJLEZ7Y";
+    $useraccount = "piyawad.k@udru.ac.th";
+
+    $totp = new \OTPHP\TOTP($secretcode);
+    $chl = $totp->provisioning_uri($useraccount); 
     echo "url  ".$chl;
 
     // 'https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/piyawad?secret=JDDK4U6G3BJLEZ7Y
